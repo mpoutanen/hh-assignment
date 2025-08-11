538e13bda58ed9536146b310aeab56183120d8f0
Mikko Poutanen <paperimappi@gmail.com>
2025-08-11 10:35:12 +0300

    feat: update graphql schema, update model location

--- packages/server/src/graphql/index.ts
+++ packages/server/src/graphql/index.ts
@@ -1,12 +1,22 @@
-import { buildSchema } from 'graphql';
-import { User } from '../handlers/db/users/models';
-import { Context } from '../types/global';
+import { buildSchema } from "graphql";
+import { User } from "../models/user";
+import { Context } from "../types/global";
+
+// GraphQL schema definition
+// Query type would benefit from pagination and filtering in a real-world scenario
+// For our needs, we fetch all users based on departmentId
 
 export const graphQLSchema = buildSchema(`
 
   type Department {
     id: ID!
     name: String!
+  }
+
+  type DepartmentSummary {
+    id: ID!
+    name: String!
+    userCount: Int!
   }
 
   type User {
@@ -17,18 +27,19 @@ export const graphQLSchema = buildSchema(`
   }
 
   type Query {
-    users: [User!]!
+    users(departmentId: ID!): [User!]!
+    departments: [DepartmentSummary!]!
   }
 `);
 
 const addUserResolvers = (ctx: Context) => (users: User[]) =>
-  users.map(user =>
+  users.map((user) =>
     Object.assign(user, {
       department: () =>
         ctx.handlers.db.department.getById(ctx, {
           id: user.departmentId,
         }),
-    }),
+    })
   );
 
 // Passing in ctx to all resolvers for dependency injection
